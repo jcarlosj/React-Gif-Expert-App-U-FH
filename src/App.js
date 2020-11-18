7dd3527b7f6f6cf8b01808988c1e063eb18babc2
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2020-11-18 15:10:28 -0500

    [+] AppComponent: Listado de Categorias

--- src/App.js
+++ src/App.js
@@ -1,10 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const App = () => {
+
+    const [ categories, setCategories ] = useState(
+        [ 'Movies', 'Series', 'Comics' ]
+    );
+
+    const handleAddCategoryOne = () => {
+        /** Forma 1 */
+        setCategories([ 
+            ...categories,
+            'Cartoon' 
+        ]);
+
+    }
+
+    const handleAddCategoryTwo = () => {
+        /** Forma 2 */
+        setCategories( cats => [ ...cats, 'Ads' ]);
+    }
+
     return (  
         <>
             <h2>GifExpertApp</h2>
             <hr />
+            <button onClick={ handleAddCategoryOne }>Add 1</button>
+            <button onClick={ handleAddCategoryTwo }>Add 2</button>
+            <ol>
+                {   categories.map( ( category, idx ) => {
+                        return <li key={ idx }>{ category }</li>;
+                    }) 
+                }
+            </ol>
         </>
     );
 }
