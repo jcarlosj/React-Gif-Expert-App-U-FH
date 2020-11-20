f1531be1fae44b628357eea44a66fbfe021f2904
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2020-11-20 16:08:34 -0500

    [+] CategoryItemTest: Define 'debe teber titulo en etiqueta h4'

--- src/tests/components/CategoryItem.test.js
+++ src/tests/components/CategoryItem.test.js
@@ -5,20 +5,27 @@ import { CategoryItem } from '../../components/CategoryItem';
 
 describe( '<CategoryItem />', () => {
 
+    const
+    image = {
+        id: 'ouyy43y4r734',
+        title: 'Once Upon a Time',
+        url: 'https://media4.giphy.com/media/IBzHGPbQLV5Fm/giphy.gif?cid=3a428325tnk0tt3ae9ee85i0zam2aldtbizw22uyqpllhw15&rid=giphy.gif'
+    }, 
+    wrapper = shallow(
+        <CategoryItem { ...image } />
+    );
+
     test( 'debe desplegar correctamente', () => {
-        
-        const
-            image = {
-                id: 'ouyy43y4r734',
-                title: 'Once Upon a Time',
-                url: 'https://media4.giphy.com/media/IBzHGPbQLV5Fm/giphy.gif?cid=3a428325tnk0tt3ae9ee85i0zam2aldtbizw22uyqpllhw15&rid=giphy.gif'
-            }, 
-            wrapper = shallow(
-                <CategoryItem { ...image } />
-            );
         // console.log( wrapper.debug() );
 
         expect( wrapper ).toMatchSnapshot();    //  Verifica que un valor coincida con la instantánea (Snapshop) más reciente
+
+    });
+
+    test( 'debe tener titulo en etiqueta h4', () => {
+
+        const $h4 = wrapper.find( 'h4' );        
+        expect( $h4.text().trim() ).toBe( image.title );
 
     });
 
