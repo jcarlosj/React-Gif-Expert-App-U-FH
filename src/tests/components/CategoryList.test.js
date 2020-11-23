8646826f715d7731f7d23ad56fa3ae01aa06f217
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2020-11-23 16:47:36 -0500

    [+] CategoryListTest: Define 'debe mostrar registros cuando las imagenes han cargado useFetchGifs'

--- src/tests/components/CategoryList.test.js
+++ src/tests/components/CategoryList.test.js
@@ -3,23 +3,58 @@ import { shallow } from 'enzyme';
 
 import { CategoryList } from '../../components/CategoryList';
 
+import { useFetchGifs } from '../../hooks/useFecthGifs';
+
+jest.mock( '../../hooks/useFecthGifs' );        //  Simula un módulo con una versión simulada automáticamente cuando se requiere
+
 describe( '<CategoryList />', () => {
 
     const
         category = 'Game of Thrones',
-        key = 'uiey82uy82fe',
-        wrapper = shallow(
+        key = 'uiey82uy82fe';
+
+    test( 'debe desplegar correctamente', () => {
+
+        useFetchGifs.mockReturnValue({         //  Valor que se devolverá siempre que se llame a la función simulada
+            loading: true,
+            data: []
+        });
+
+        const wrapper = shallow(
             <CategoryList 
                 key={ key } 
                 category={ category } 
             />
         );
 
-    test( 'debe desplegar correctamente', () => {
         // console.log( wrapper.debug() );
 
         expect( wrapper ).toMatchSnapshot();    //  Verifica que un valor coincida con la instantánea (Snapshop) más reciente
 
     });
+
+    test( 'debe mostrar registros cuando las imagenes han cargado \'useFetchGifs\'', () => {
+
+        const gifs = [{
+            id: 'iiuyuidfsf454rdf',
+            title: 'The Simpsons',
+            url: 'http://localhost/cualquier/imagen.jpg'
+        }];
+
+        useFetchGifs.mockReturnValue({         //  Valor que se devolverá siempre que se llame a la función simulada
+            loading: false,
+            data: gifs
+        });
+
+        const wrapper = shallow(
+            <CategoryList 
+                key={ key } 
+                category={ category } 
+            />
+        );
+
+        expect( wrapper ).toMatchSnapshot();    //  Verifica que un valor coincida con la instantánea (Snapshop) más reciente
+
+    }); 
 
 });
