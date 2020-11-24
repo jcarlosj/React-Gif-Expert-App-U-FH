3d4814186777606b13cd27e658a5e06c85639155
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2020-11-24 10:02:40 -0500

    [+] useFetchGifs.test: Define 'debe retornar el estado inicial' - usando @testing-library/react-hooks

--- src/tests/hooks/useFecthGifs.test.js
+++ src/tests/hooks/useFecthGifs.test.js
@@ -1,4 +1,5 @@
 import '@testing-library/jest-dom';
+import { renderHook } from '@testing-library/react-hooks';
 import { shallow } from 'enzyme';
 
 import { useFetchGifs } from '../../hooks/useFecthGifs';
@@ -7,8 +8,14 @@ describe( 'Hook: useFetchGifs', () => {
 
     test( 'debe retornar el estado inicial', () => {
         
-        const { loading, data:images } = useFetchGifs( 'Superman' );
-        console.log( loading, images );
+        const 
+            { result } = renderHook( () => useFetchGifs( 'Superman' ) ),       //  renderHook(): Renderiza el Hook (debemos pasar el Hook como el retorno de una funcion)
+            { loading, data } = result.current;
+        
+        // console.log( loading, data );
+
+        expect( data ).toEqual( [] );           //  data debe ser un Array vacio
+        expect( loading ).toBe( true );         //  loading debe ser true
 
     });
     
